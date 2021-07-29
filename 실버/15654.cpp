a46d5b891cf7bbf309fd2d75179d289d8f487d91
GodSang <tkdgh584@gmail.com>
2021-07-29 20:17:25 +0900

    [백트래킹] 21/07/29 (Silver3)

---
+++ 실버/15654.cpp
@@ -0,0 +1,36 @@
+// 20:09 ~ 20:17
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+vector<int> num;
+int arr[9];
+bool chk[9];
+int n,m;
+
+void func(int cur) {
+    if(cur==m) {
+        for(int i=0;i<m;i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    for(int i=0;i<n;i++) {
+        if(chk[i] == 1) continue;
+        chk[i] = 1;
+        arr[cur] = num[i];
+        func(cur+1);
+        chk[i] = 0;
+    }
+}
+
+int main() {
+    int k;
+    cin >> n >> m;
+    for(int i=0;i<n;i++) {
+        cin >> k;
+        num.push_back(k);
+    }
+    sort(num.begin(), num.end());
+    func(0);
+}
